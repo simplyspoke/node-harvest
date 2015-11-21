f3d026b19730ac90f7f9a0d1f3c17ad164b76cd4
dnsl48 <dnsl48@gmail.com>
2015-11-22 08:55:36 +1300

    Harvest.Clients.update bugfix; Improve clients-api-tests: integrational testing implementation

--- test/clients-api-tests.js
+++ test/clients-api-tests.js
@@ -8,7 +8,26 @@ var assert = require('assert'),
     }),
     Clients = harvest.Clients;
 
+
+var TEST_CLIENT_NAME = '__NODE-HARVEST__TESTS__CLIENT__',
+    TEST_CLIENT_ID = null;
+
+
 describe('The Clients API', function() {
+    describe('Create a new client', function() {
+        it('should implement the create method', function() {
+            assert.equal(typeof Clients.create, "function");
+        });
+        it('create method should work properly', function(done) {
+            Clients.create({ "client": { "name": TEST_CLIENT_NAME } }, function (err, data) {
+                if (err)
+                    assert.equal(err.message, 'Name has already been taken');
+                else
+                    assert(data);
+                done();
+            });
+        });
+    });
     describe('Get all clients', function() {
         it('should implement the list method', function() {
             assert.equal(typeof Clients.list, "function");
@@ -16,6 +35,15 @@ describe('The Clients API', function() {
 	it('should provide a list of all clients', function(done) {
 	    Clients.list({}, function(err, clients) {
 		assert(!err);
+                var testClient = undefined;
+                for (var i = 0; i < clients.length; ++i) {
+                    if (clients[i].client.name === TEST_CLIENT_NAME) {
+                        testClient = clients[i].client;
+                        break;
+                    }
+                }
+                assert(testClient);
+                TEST_CLIENT_ID = testClient.id;
 		done();
 	    });
 	});
@@ -24,25 +52,68 @@ describe('The Clients API', function() {
         it('should implement the get method', function() {
             assert.equal(typeof Clients.get, "function");
         });
-    });
-    describe('Create a new client', function() {
-        it('should implement the create method', function() {
-            assert.equal(typeof Clients.create, "function");
+        it('get method should work properly', function(done) {
+            assert(TEST_CLIENT_ID);
+            Clients.get({"id": TEST_CLIENT_ID}, function(err, data) {
+                assert(!err);
+                assert(data);
+                assert(data.client);
+                assert.equal(data.client.id, TEST_CLIENT_ID);
+                assert.equal(data.client.name, TEST_CLIENT_NAME);
+                done();
+            });
         });
     });
     describe('Update client', function() {
         it('should implement the update method', function() {
             assert.equal(typeof Clients.update, "function");
         });
+        it('update method should work properly', function(done) {
+            assert(TEST_CLIENT_ID);
+            var dets = "New details: " + Math.random();
+
+            Clients.update({"id": TEST_CLIENT_ID, "client": { "name": TEST_CLIENT_NAME, "details": dets } }, function (err, data, res) {
+                assert(!err);
+                Clients.get({"id": TEST_CLIENT_ID}, function(err, data) {
+                    assert(!err);
+                    assert(data);
+                    assert(data.client);
+                    assert.equal(data.client.id, TEST_CLIENT_ID);
+                    assert.equal(data.client.name, TEST_CLIENT_NAME);
+                    assert.equal(data.client.details, dets);
+                    done();
+                });
+            });
+        });
     });
     describe('(De)Activate an existing client', function() {
         it('should implement the toggleActivation method', function() {
             assert.equal(typeof Clients.toggleActivation, "function");
         });
+        it('toggleActivation method should work properly', function(done) {
+            assert(TEST_CLIENT_ID);
+            Clients.toggleActivation({ id: TEST_CLIENT_ID }, function(err, data) {
+                assert(!err);
+                Clients.get({ id: TEST_CLIENT_ID }, function (err, data) {
+                    assert(!err);
+                    assert(data);
+                    assert(data.client);
+                    assert(!data.client.active);
+                    done();
+                });
+            });
+        });
     });
     describe('Delete a client', function() {
         it('should implement the delete method', function() {
             assert.equal(typeof Clients.delete, "function");
+        });
+        it('delete method should work properly', function(done) {
+            assert(TEST_CLIENT_ID);
+            Clients.delete({ id: TEST_CLIENT_ID }, function (err, data) {
+                assert(!err);
+                done();
+            });
         });
     });
 });

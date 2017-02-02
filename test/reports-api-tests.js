2bb331382d6d86256ba988588b36a4dbf6a4031e
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-02-02 13:32:09 -0600

    Cleaned up the api and started a base mixin

--- test/reports-api-tests.js
+++ test/reports-api-tests.js
@@ -6,16 +6,6 @@ const common = require('./common');
 const harvest = common.harvest;
 
 describe('The Reports API', function() {
-  describe('Get all time entries logged to a project for a given timeframe', function() {
-    it('should implement the timeEntriesByProject method', function() {
-      assert.equal(typeof harvest.reports.timeEntriesByProject, 'function');
-    });
-  });
-  describe('Get all time entries by the current user logged to a project for a given timeframe', function() {
-    it('should implement the timeEntriesByCurrentUser method', function() {
-      assert.equal(typeof harvest.reports.timeEntriesByCurrentUser, 'function');
-    });
-  });
   describe('Get all time entries logged by a user for a given timeframe', function() {
     it('should implement the timeEntriesByCurrentUser method', function() {
       assert.equal(typeof harvest.reports.timeEntriesByUser, 'function');
@@ -26,14 +16,14 @@ describe('The Reports API', function() {
       assert.equal(typeof harvest.reports.expensesByUser, 'function');
     });
   });
+  describe('Get all time entries logged to a project for a given timeframe', function() {
+    it('should implement the timeEntriesByProject method', function() {
+      assert.equal(typeof harvest.reports.timeEntriesByProject, 'function');
+    });
+  });
   describe('Get all expenses entries to a project for a given timeframe', function() {
     it('should implement the expensesByProject method', function() {
       assert.equal(typeof harvest.reports.expensesByProject, 'function');
-    });
-  });
-  describe('Get all expenses entries to a client for a given timeframe', function() {
-    it('should implement the expensesByClient method', function() {
-      assert.equal(typeof harvest.reports.expensesByClient, 'function');
     });
   });
 });

3344cdce0b6d6071a6e7077f6c81ece39f20a83a
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-02-02 15:07:12 -0600

    Added some minor coverage enhancement

--- test/harvest-client-tests.js
+++ test/harvest-client-tests.js
@@ -4,6 +4,8 @@ const assert = require('assert');
 const config = require('./common').config;
 const Harvest = require('../lib/harvest');
 
+const harvest = require('./common').harvest;
+
 describe('The Harvest API Client', function() {
   describe('Instantiating a Harvest instance', function() {
     it('should be able to work with HTTP basic authentication', function() {
@@ -22,6 +24,19 @@ describe('The Harvest API Client', function() {
         redirectUri: config.redirectUri
       });
       assert(typeof harvest === 'object');
+    });
+  });
+  describe('Get an access code url', function() {
+    it('should implement the get method', function() {
+      assert.equal(typeof harvest.getAccessTokenURL, 'function');
+    });
+    it('should return a string', function() {
+      assert.equal(typeof harvest.getAccessTokenURL(), 'string');
+    });
+  });
+  describe('Parse as access code', function() {
+    it('should implement the get method', function() {
+      assert.equal(typeof harvest.parseAccessCode, 'function');
     });
   });
 });

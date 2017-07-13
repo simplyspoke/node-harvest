cf4ad2030cfc723e00d583edad39611a982c5912
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-07-12 22:15:18 -0500

    Issue #66 - Patched the clientContacts.listByClient function.
    upate

--- test/client-contacts-api-tests.js
+++ test/client-contacts-api-tests.js
@@ -2,13 +2,25 @@
 
 const assert = require('assert');
 const common = require('./common');
+const helpers = require('../lib/helpers');
 
 const harvest = common.harvest;
 let clients;
 
+const random = parseInt(Math.random() * 10000, 10);
+const TEST_CONTACT_NAME2 = '__NODE-HARVEST__TESTS__CONTACT__' + random;
+const TEST_CLIENT_NAME2 = '__NODE-HARVEST__TESTS__CLIENT__' + random;
+
+let TEST_CONTACT_ID2;
+let TEST_CLIENT_ID2;
+
 describe('The ClientContacts API', function() {
   before(function(done) {
-    loadIds(done);
+    seedHarvest(done);
+  });
+
+  after(function(done) {
+    cleanupHarvest(done);
   });
   describe('Get all client contacts for an account', function() {
     it('should implement the list method', function() {
@@ -20,7 +32,7 @@ describe('The ClientContacts API', function() {
       assert.equal(typeof harvest.clientContacts.listByClient, 'function');
     });
     it('should return an array of contacts', function() {
-      harvest.clientContacts.listByClient(clients[0].client.id, function(err, res, contacts) {
+      harvest.clientContacts.listByClient(TEST_CLIENT_ID2, function(err, res, contacts) {
         assert(typeof contacts, 'array');
       });
     });
@@ -52,9 +64,35 @@ describe('The ClientContacts API', function() {
   });
 });
 
-function loadIds(done) {
-  harvest.clients.list({}, function(err, res, results) {
-    clients = results;
-    done();
+
+function seedHarvest(done) {
+  harvest.clients.create({
+    'client': {
+      'name': TEST_CLIENT_NAME2,
+      'active': true,
+      'currency': 'United States Dollar - USD',
+      'currency_symbol': '$',
+      'details': '123 Main St\r\nAnytown, NY 12345'
+    }
+  }, function(err, res, body) {
+    TEST_CLIENT_ID2 = helpers.getId(res);
+    harvest.clientContacts.create({
+      'contact': {
+        'client_id': TEST_CLIENT_ID2,
+        'first_name': TEST_CONTACT_NAME2,
+        'last_name': TEST_CONTACT_NAME2
+      }
+    }, function(err, res, body) {
+      TEST_CONTACT_ID2 = helpers.getId(res);
+      done();
+    });
+  });
+}
+
+function cleanupHarvest(done) {
+  harvest.clientContacts.delete(TEST_CONTACT_ID2, function() {
+    harvest.clients.delete(TEST_CLIENT_ID2, function() {
+      done();
+    });
   });
 }

05b1b0b1a0a8bdfe5c25585fb1d9a5311c342b5e
Tristan <tristan@simplyspoke.com>
2018-03-06 10:22:24 -0600

    docs(Added Docs): Added in the documentation generator to the project.

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class","1024":"Property"},"rows":[{"id":0,"kind":128,"name":"DummyClass","url":"classes/dummyclass.html","classes":"tsd-kind-class"},{"id":1,"kind":1024,"name":"test","url":"classes/dummyclass.html#test","classes":"tsd-kind-property tsd-parent-kind-class","parent":"DummyClass"}]};

c2c6d3471858f51762de940873c7971aa9b093a5
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2021-04-01 03:48:56 +0200

    refactor(docs): rework documentation tooling
    There's no need to run the docs generation on every single build, instead, we can just run it when
    we want to actually deploy or view the docs

---
+++ tools/gh-pages-publish.js
@@ -0,0 +1,34 @@
+const { cd, exec, echo, touch } = require('shelljs');
+const { readJsonSync } = require('fs-extra');
+const url = require('url');
+
+require('dotenv').config();
+
+let repoUrl;
+let pkg = readJsonSync('package.json');
+if (typeof pkg.repository === 'object') {
+  if (!pkg.repository.hasOwnProperty('url')) {
+    throw new Error('URL does not exist in repository section');
+  }
+  repoUrl = pkg.repository.url;
+} else {
+  repoUrl = pkg.repository;
+}
+
+let parsedUrl = url.parse(repoUrl);
+let repository = (parsedUrl.host || '') + (parsedUrl.path || '');
+let ghToken = process.env.GH_TOKEN;
+
+echo('Deploying docs!!!');
+exec('yarn docs:generate');
+cd('docs');
+touch('.nojekyll');
+exec('git init');
+exec('git add .');
+exec('git config user.name "Tristan Fitzgerald"');
+exec('git config user.email "tristan@simplyspoke.com"');
+exec('git commit -m "docs: update gh-pages"');
+exec(
+  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+);
+echo('Docs deployed!!');
